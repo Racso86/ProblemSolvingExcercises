ad9438fdd9864515518ddbda691f45d3ab24c3dc
Oscar Jemio <oscar.jemio@trueforce.com>
2022-09-02 14:57:33 -0400

    1st commit

---
+++ problem1.cs
@@ -0,0 +1,69 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+     /*
+     HACKERRANK
+     LINK 
+     https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        int level = 0;
+        int valleys =0;
+        var paths = path.ToArray();
+        for (int i = 0; i < steps; i++)
+        {
+            if (level == 0 && paths[i]=='D')
+            {
+                valleys++;
+            }
+            if (paths[i] == 'D') level--;
+            else level++;
+        }
+        return valleys;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
